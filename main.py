cc3a9d6a8d47ed8bb65f3f7b12d7767df0a9f568
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2024-03-16 02:11:44 -0400

    tried another method

--- main.py
+++ main.py
@@ -4,25 +4,37 @@ from mfrc522 import MFRC522
 from time import sleep
 
 
-# reader = SimpleMFRC522()
+# reader2 = SimpleMFRC522()
 print("="*15 + "\nREADER LIVE\n")
 
 # try:
 #     while (True): 
-#         # id, text= reader.read()
+#         id = reader2.read_id()
 #         print("ID:", id)
-#         print("Text:",text)
-#         print
+#         # print("Text:",text)
+#         values = reader
 # finally:
 #     GPIO.cleanup()
 
 
 reader = MFRC522()
 status = None
-while true:
-    (status,TagType) = reader.Request(reader.PICC_REQIDL)
-    if status != reader.MI_OK:
-        sleep(0.1)
-        continue
-    status, backData = reader.MFRC522_Anticoll()
-    print(backData)
+try: 
+    while True:
+        (status,TagType) = reader.MFRC522_Request(reader.PICC_REQIDL)
+        if status != reader.MI_OK:
+            sleep(0.1)
+            continue
+        status, backData = reader.MFRC522_Anticoll()
+        print(backData)
+        # print(reader.MFRC522_Read())
+        blocks = [0,1,2,3]
+        data = []
+        for block_num in blocks:
+            block_data = reader.MFRC522_Read(block_num)
+            if block_data:
+                data += block_data
+        if data:
+            print(''.join(chr(i) for i in data))
+finally:
+    GPIO.cleanup()
