3007aa80b39a9a4f5839ebbcdc9b6f2d3b0969ba
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2024-03-16 00:38:04 -0400

    Edited main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
-reader = SimpleMFRC522()
 
-try:
-    id, text= reader.read()
-    print(id)
-    print(text)
-finally:
-    GPIO.cleanup()
+reader = SimpleMFRC522()
+print("="*6 + "\nREADER LIVE\n")
+
+while (True): 
+    try:
+        id, text= reader.read()
+        print(id)
+        print(text)
+    finally:
+        GPIO.cleanup()
