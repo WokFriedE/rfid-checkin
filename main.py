eed151a59b61aaa337871cc11d19980f979cc67c
Ethan H <47959663+WokFriedE@users.noreply.github.com>
2024-03-16 02:15:23 -0400

    Streamlined Reading

--- main.py
+++ main.py
@@ -1,40 +1,17 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
-from mfrc522 import MFRC522
-from time import sleep
+import time 
 
 
-# reader2 = SimpleMFRC522()
 print("="*15 + "\nREADER LIVE\n")
 
-# try:
-#     while (True): 
-#         id = reader2.read_id()
-#         print("ID:", id)
-#         # print("Text:",text)
-#         values = reader
-# finally:
-#     GPIO.cleanup()
-
-
-reader = MFRC522()
-status = None
-try: 
-    while True:
-        (status,TagType) = reader.MFRC522_Request(reader.PICC_REQIDL)
-        if status != reader.MI_OK:
-            sleep(0.1)
-            continue
-        status, backData = reader.MFRC522_Anticoll()
-        print(backData)
-        # print(reader.MFRC522_Read())
-        blocks = [0,1,2,3]
-        data = []
-        for block_num in blocks:
-            block_data = reader.MFRC522_Read(block_num)
-            if block_data:
-                data += block_data
-        if data:
-            print(''.join(chr(i) for i in data))
+reader2 = SimpleMFRC522()
+try:
+    while (True): 
+        id = reader2.read_id()
+        t = time.localtime()
+        curr_time = time.strftime("%H:%M:%S", t) 
+        print("ID", curr_time+":", id)
 finally:
     GPIO.cleanup()
+
